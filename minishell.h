21f11f64946c2e1b464b4d314e618a7a48d3a142
wzakkabi <walid1997e@gmail.com>
2023-07-14 19:32:37 +0100

    start minishell

---
+++ minishell.h
@@ -0,0 +1,19 @@
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+#include <readline.h>
+#include <history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <dirent.h>
+#include <string.h>
+#include <errno.h>
+#include <termios.h>
+#include <termcap.h>
+
+# endif
