da537903de2671fff8a43f1e87943df2137bf18e
mouad <bousbaamouad@gmail.com>
2023-09-24 21:02:26 +0100

    execution, multiple pipes between commands almost done needs some errors handling

--- execution.c
+++ execution.c
@@ -6,7 +6,7 @@
 /*   By: mbousbaa <mbousbaa@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 16:06:31 by mbousbaa          #+#    #+#             */
-/*   Updated: 2023/09/19 18:56:17 by mbousbaa         ###   ########.fr       */
+/*   Updated: 2023/09/24 00:48:17 by mbousbaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -64,37 +64,57 @@ void	check_redirections(t_lexer *lexer, int *fds)
 	
 }
 
-int	execute_cmd(t_ast *ast, t_env *env, int	*pipe_fd)
+int	execute_cmd(t_ast *ast, t_env *env)
 {
 	int		child;
-	int		i, fd;
+	int		i, pipe_fd[2];
 	char	**bin_paths;
 	char	*tmp;
+	static int save;
 
-	if (ast == NULL)
-		return 0;
-	if (ast->prev != NULL)
-	{
-		// dup2(pipe_fd[0], STDIN_FILENO);
-		// close(pipe_fd[1]);
-		close(pipe_fd[1]);
-	}
+	pipe(pipe_fd);
+
+	// if (ast == NULL)
+	// 	return 0;
+	// if (ast->prev != NULL)
+	// {
+	// 	// dup2(pipe_fd[0], STDIN_FILENO);
+	// 	// close(pipe_fd[1]);
+	// 	close(pipe_fd[1]);
+	// }
 	child = fork();
 	if (child == 0)
 	{
-		if (ast->next != NULL || (ast->redirections != NULL
-			&& ast->redirections->token == GREAT))
-		{
-			close(pipe_fd[0]);
-			dup2(pipe_fd[1], STDOUT_FILENO);
-			close(pipe_fd[1]);
+		// getchar();
+		// if (ast->next != NULL /*|| (ast->redirections != NULL
+		// 	&& ast->redirections->token == GREAT)*/)
+		// {
+		// 	close(pipe_fd[0]);
+		// 	dup2(pipe_fd[1], STDOUT_FILENO);
+		// 	close(pipe_fd[1]);
+		// }
+		// else if (ast->prev != NULL)
+		// {
+		// 	close(pipe_fd[1]);
+		// 	dup2(pipe_fd[0], STDIN_FILENO);
+		// 	close(pipe_fd[0]);
+		// }
+
+		if (!ast->prev) {
+			dup2(pipe_fd[1], 1);
 		}
-		else if (ast->prev != NULL)
+		else if (ast->prev && ast->next)
 		{
-			close(pipe_fd[1]);
-			dup2(pipe_fd[0], STDIN_FILENO);
-			close(pipe_fd[0]);
+			dup2(save, 0);
+			dup2(pipe_fd[1], 1);
 		}
+		else if (!ast->next) {
+			dup2(save, 0);
+		}
+		
+		close(pipe_fd[0]);
+		close(pipe_fd[1]);
+		close(save);
 		bin_paths = get_bin_paths(env);
 		i = -1;
 		while (bin_paths[++i])
@@ -106,7 +126,11 @@ int	execute_cmd(t_ast *ast, t_env *env, int	*pipe_fd)
 	}
 	else
 	{
-		check_redirections(ast->redirections, pipe_fd);
+		if (save > 2)
+			close(save);
+		save = pipe_fd[0];
+		close(pipe_fd[1]);
+		// check_redirections(ast->redirections, pipe_fd);
 		return child;
 	}
 }
@@ -116,21 +140,24 @@ void	execute(t_ast *ast, t_env *env)
 	t_ast	*ast_p;
 	int		child;
 	int		state;
-	int		pipe_fd[2];
+	// int		pipe_fd[2];
 
 	
-	pipe(pipe_fd);
 	ast_p = ast;
 	while (ast_p)
 	{	
 		// if (ast->builtins != 0)
 		// 	builtin(ast, env);
 		// else
-			child = execute_cmd(ast_p, env, pipe_fd);
-		if (child != 0)
-			waitpid(child, &state ,0);
+		// if (ast_p->redirections != NULL)
+		// 	check_redirections(ast_p->redirections);
+			
+			child = execute_cmd(ast_p, env);
+		// if (child != 0)
+		// 	waitpid(child, &state ,0);
 		ast_p = ast_p->next;
 	}
-	close (pipe_fd[1]);
-	close (pipe_fd[0]);
+	// waitpid(child, &state ,0);
+	// close (pipe_fd[1]);
+	// close (pipe_fd[0]);
 }
--- redirections.c
+++ redirections.c
@@ -6,7 +6,7 @@
 /*   By: mbousbaa <mbousbaa@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 17:55:42 by mbousbaa          #+#    #+#             */
-/*   Updated: 2023/09/19 19:23:57 by mbousbaa         ###   ########.fr       */
+/*   Updated: 2023/09/23 22:14:54 by mbousbaa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,14 +25,17 @@ void	great(t_lexer *lexer, int *pipe_fds)
 	fd = open(lexer->word, O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR);
 	if (fd <= 2)
 		return ;
-	count = read(pipe_fds[0], buff, BUFSIZ);
-	if (count > 0)
-	{
-		// buff[count] = '\0';
-		int w_ret = write(fd, buff, count);
-		if (w_ret <= 0)
-			perror("write()");
+	// close(pipe_fds[1]);
+	// count = read(pipe_fds[0], buff, BUFSIZ);
+	// close(pipe_fds[0]);
+	// if (count > 0)
+	// {
+	// 	// buff[count] = '\0';
+	// 	int w_ret = write(fd, buff, count);
+	// 	if (w_ret <= 0)
+	// 		perror("write()");
 			
-	}
+	// }
+	dup2(fd, STDOUT_FILENO);
 	close(fd);
 }
