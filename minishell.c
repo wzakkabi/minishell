a8a02fc7401bf6784530816cc3c46e55326952c2
walid <walid1997e@gmail.com>
2023-09-29 17:55:05 -0400

    expand done test and if you find something told me

--- minishell.c
+++ minishell.c
@@ -283,21 +283,32 @@ void	check_syntax_error(t_lexer *err)
 		err = err->next;
 	}
 }
-
-void	check_expand(t_lexer *token, t_env *env)
+int how_many(char *s, int c)
 {
-	int x = 0, y = 0, i = 0;
-	int c_p_dollar = 0, c_p_key = 0;
-	char *key;
-	char *new_word;
-	t_env *test;
+	int x = 0;
+	int find = 0;
 
-	while(token->next != NULL)
+	if(!s || s[0] == 39)
+		return 0;
+	while(s[x])
 	{
-		if(ft_strrchr(token->word, '$') != NULL && token->word[0] != 39 && token->word)
+		if(s[x] == c)
+			find++;
+		x++;
+	}
+	return find;
+}
+
+/*
+while(token->next != NULL)
+	{//&& token->word[x] || token->word[x + 1] != ' ' && token->word[x] == '$' && token->word[x]
+
+		dollar = how_many(token->word, '$');
+		while(dollar != 0&& token->word)
 		{
-			while(token->word[x] != '$')
+			while(token->word[x] != '$' )
 				x++;
+			printf("%d,\n", x);
 			c_p_dollar = ++x;
 			c_p_key = 0;
 			while(token->word[x + c_p_key] != ' ' && token->word[x + c_p_key] != '$' && token->word[x + c_p_key] != '\t' && token->word[x + c_p_key] && token->word[x + c_p_key] != 34)
@@ -329,18 +340,80 @@ void	check_expand(t_lexer *token, t_env *env)
 			free(token->word);
 			free(key);
 			token->word = new_word;
-
+			dollar--;
+			c_p_dollar = 0;
+			c_p_key = 0;
+			y = 0;
+			i = 0;
+			x = 0;
 		}
-		else
-		{
-			token = token->next;
-		}
-		c_p_dollar = 0;
-		c_p_key = 0;
-		y = 0;
 		x = 0;
-		i = 0;
+		token = token->next;
 	}
+*/
+
+void	check_expand(t_lexer *token, t_env *env)
+{
+	int x = 0, y = 0, i = 0, dollar = 0;
+	int c_p_dollar = 0, c_p_key = 0;
+	char *key;
+	char *new_word;
+	t_env *test;
+
+	while(token->next)
+	{
+		dollar = how_many(token->word, '$');
+		while(dollar != 0)
+		{
+			while(token->word[x] != '$')
+				x++;
+			if(token->word[x + 1] == ' ' || token->word[x + 1] == '\t' || token->word[x + 1] == '\0' || token->word[x + 1] == 34)
+				x++;
+			else
+			{
+				c_p_dollar = ++x;
+				c_p_key = 0;
+				while(token->word[x + c_p_key] != ' ' && token->word[x + c_p_key] != '$' && token->word[x + c_p_key] != '\t' && token->word[x + c_p_key] && token->word[x + c_p_key] != 34)
+					c_p_key++;
+				key = ft_substr2(token->word, c_p_dollar, c_p_key + x);
+				test = get_env_var(env, key);
+				if(test)
+				{
+					new_word = malloc((ft_strlen(token->word) - c_p_key) + ft_strlen(test->value));
+					c_p_dollar = 0;
+					while(c_p_dollar < x - 1)
+						new_word[y++] = token->word[c_p_dollar++];					
+					while(test->value[i])
+						new_word[y++] = test->value[i++];
+					while(token->word[c_p_dollar + c_p_key])
+						new_word[y++] = token->word[c_p_dollar + ++c_p_key];
+					new_word[y] = 0;
+				}
+				else
+				{
+					new_word = malloc((ft_strlen(token->word) - c_p_key));
+					c_p_dollar = 0;
+					while(c_p_dollar < x - 1)
+						new_word[y++] = token->word[c_p_dollar++];
+					while(token->word[c_p_dollar + c_p_key])
+						new_word[y++] = token->word[c_p_dollar + ++c_p_key];
+					new_word[y] = 0;
+				}
+				free(token->word);
+				free(key);
+				token->word = new_word;
+				dollar--;
+				c_p_dollar = 0;
+				c_p_key = 0;
+				y = 0;
+				i = 0;
+			}
+			dollar = how_many(token->word + x, '$');
+		}
+		token = token->next;
+		dollar = 0;
+	}
+	
 }
 
 void	remove_qost(t_lexer *token)
@@ -385,9 +458,7 @@ void    minishell_loop(t_ast *tool, t_lexer *token, t_env *env)
 	check_expand(token, env);
 	remove_qost(token);
 	tool = split_to_ast(token);
-	 ft_printast(tool);
-	// while(tool->prev != NULL)
-		// tool = tool->prev;
+	//ft_printast(tool);
 	execute(tool, env);
 }
 
