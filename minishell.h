fdba3cf84a0b1fa63423f5adaf668feb34ec3b69
Walid Zakkabi <wzakkabi@c1r8p6.1337.ma>
2023-10-22 12:27:35 +0100

    norm

--- minishell.h
+++ minishell.h
@@ -1,23 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wzakkabi <wzakkabi@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/22 12:10:20 by wzakkabi          #+#    #+#             */
+/*   Updated: 2023/10/22 12:26:49 by wzakkabi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-
-#include "./libft/libft.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <dirent.h>
-#include <string.h>
-#include <errno.h>
-#include <termios.h>
-#include <termcap.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include <sys/wait.h>
+# include "./libft/libft.h"
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <signal.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <termios.h>
+# include <termcap.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <sys/wait.h>
 
 typedef enum s_token
 {
@@ -26,54 +36,58 @@ typedef enum s_token
 	LESS,
 	LESS_LESS,
 	PIPE,
-} t_token;
+}	t_token;
 
 typedef struct s_lexer
 {
-	char    	*word;
-	t_token       token;
-	int			num_node;
+	char			*word;
+	t_token			token;
+	int				num_node;
 	struct s_lexer	*next;
 	struct s_lexer	*prev;
 }	t_lexer;
 
-
+// int builtins = 0 false o 1 true
+//t_lexer	*redirections; = here is all the node of redirection
+//t_lexer	*token = i add it just to free all in one move
+// str all the commend and paramiter
 typedef struct s_ast
 {
-	char                    **str;
-	int						builtins; // 0 false o 1 true >> hadi ila kant t_ast fiha cmd li galo lina dero
-	t_lexer                 *redirections;/*hna ghadi ndir lik ga3 node li fihom redirection b tertib bhal >> > << <
-	ghadi ikom token = "redirection" and word fiha ya ima delemter or smit file */
-	t_lexer					*token;
-	struct s_ast			*next;
-	struct s_ast			*prev;
-} t_ast;
+	char			**str;
+	int				builtins;
+	t_lexer			*redirections;
+	t_lexer			*token;
+	struct s_ast	*next;
+	struct s_ast	*prev;
+}	t_ast;
 
-
+//int print_or_not; //0 or 1 ztha  bach n3erfha wach n9dero nprantiwha ola la
 typedef struct s_env 
 {
-	char *key;
-	char *value;
-	int print_or_not; //0 or 1 ztha  bach n3erfha wach n9dero nprantiwha ola la
-	struct s_env *next;
-	struct s_env *prev;
-} t_env;
-
+	char			*key;
+	char			*value;
+	int				print_or_not;
+	struct s_env	*next;
+	struct s_env	*prev;
+}	t_env;
 
 // minishell.c
 void	minishell_loop(t_env *env);
-int	main(int c, char **av, char **grep_env);
+int		main(int c, char **av, char **grep_env);
+
 /**** exit_status ****/
-void ft_exit(t_env *env, int return_status);
+void	ft_exit(t_env *env, int return_status);
 
 // create_node.c
 t_ast	*newnode(void);
 t_lexer	*lxnewnode(void);
 t_env	*envnode(void);
+
 // expand.c
 void	check_expand(t_lexer *token, t_env *env);
+
 // ft_substr2.c
-char	*ft_substr2(char const *s, unsigned int start, size_t len);\
+char	*ft_substr2(char const *s, unsigned int start, size_t len);
 
 // ft_token.c
 t_lexer	*ft_token(char *ret);
@@ -87,16 +101,16 @@ void	remove_qost(t_lexer *token, int x, int y, int qst);
 void	make_env_node(char **env, t_env *node);
 
 // syntax_error.c
-int	check_quote(char *str, t_env *env);
-int	check_syntax_error(t_env *env, t_lexer *err);
-int	check_syntax_error_again(t_ast *tool);
+int		check_quote(char *str, t_env *env);
+int		check_syntax_error(t_env *env, t_lexer *err);
+int		check_syntax_error_again(t_ast *tool);
 
 // ft_free.c
 void	ft_free_token(t_lexer *lx);
 void	ft_free_ast(t_ast *tool);
 
 // new readline
-void rl_replace_line(const char *text, int clear_undo);
+void	rl_replace_line(const char *text, int clear_undo);
 
 //  execution.c
 void	execute(t_ast *ast, t_env *env);
@@ -104,19 +118,4 @@ void	execute(t_ast *ast, t_env *env);
 // redirections.c
 void	overwrite_append(t_lexer *lexer, int *pipe_fds);
 
-
-void ft_exit(t_env *env, int return_status);
-
-// //error
-// int		check_quote(char *str, t_env *env);
-// int	check_syntax_error(t_env *env, t_lexer *err);
-// int		check_syntax_error_again(t_ast *tool);
-
-// //expand
-// void    check_expand(t_lexer *token, t_env *env);
-
-
-
-
-
-# endif
+#endif
