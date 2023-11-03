9308f1a01a004a49ea5d816521b0b81d91d36308
Walid Zakkabi <wzakkabi@c1r8p6.1337.ma>
2023-11-03 17:19:34 +0100

    fix sig

--- expand.c
+++ expand.c
@@ -6,7 +6,7 @@
 /*   By: wzakkabi <wzakkabi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 23:35:12 by wzakkabi          #+#    #+#             */
-/*   Updated: 2023/11/03 17:11:34 by wzakkabi         ###   ########.fr       */
+/*   Updated: 2023/11/03 17:18:40 by wzakkabi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,7 +55,7 @@ int	how_many(char *s, int c)
 
 void	expand_helper_1(t_lexer *token, t_env *test, t_ex *ex)
 {
-	if (test)
+	if (test && test->value)
 	{
 		ex->new_word = malloc((ft_strlen(token->word)
 					- ex->pk) + ft_strlen(test->value) + 1);
@@ -113,14 +113,14 @@ void	expand_helper_0(t_lexer *tk, t_env *env, t_ex *ex)
 	else
 	{
 		ex->c_p_dollar = ((ex->pk = 0), ++ex->x);
-		while ((tk->word[ex->x + ex->pk] && tk->word[ex->x + ex->pk] >= '0'
+		while ((tk->word[ex->x + ex->pk] != 0 && tk->word[ex->x + ex->pk] >= '0'
 				&& tk->word[ex->x + ex->pk] <= '9')
-			|| (tk->word[ex->x + ex->pk] && tk->word[ex->x + ex->pk] >= 'A'
+			|| (tk->word[ex->x + ex->pk] != 0 && tk->word[ex->x + ex->pk] >= 'A'
 				&& tk->word[ex->x + ex->pk] <= 'Z')
-			|| (tk->word[ex->x + ex->pk] && tk->word[ex->x + ex->pk] >= 'a'
+			|| (tk->word[ex->x + ex->pk] != 0&& tk->word[ex->x + ex->pk] >= 'a'
 				&& tk->word[ex->x + ex->pk] <= 'z')
-			|| (tk->word[ex->x + ex->pk] && tk->word[ex->x + ex->pk] == '_')
-			|| (tk->word[ex->x + ex->pk] && tk->word[ex->x + ex->pk] == '?'))
+			|| (tk->word[ex->x + ex->pk] != 0 && tk->word[ex->x + ex->pk] == '_')
+			|| (tk->word[ex->x + ex->pk] != 0 && tk->word[ex->x + ex->pk] == '?'))
 			ex->pk++;
 		ex->key = ft_substr2(tk->word, ex->c_p_dollar, ex->pk + ex->x);
 		test = get_env_var(env, ex->key);
